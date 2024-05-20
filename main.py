e242a2e7427d939a40563d9ef4ca79e5dd17a8d7
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-20 12:10:25 +0500

    Added About Page Static Folder for images templates folder for html files added website routes in main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask("Flash Website")
+
+@app.route("/")
+def home():
+    return render_template("home.html") #html files must be in "templates" folder
+@app.route("/about")
+def about():
+    return render_template("about.html") #html files must be in "templates" folder
+
+
+app.run(debug=True)
+
